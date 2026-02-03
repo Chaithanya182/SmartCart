2115a25d2ff3b638baa47b8ad43c18c15ba80a60
chaithanya <chaitanyaprasad.182@gmail.com>
2026-02-03 23:44:43 +0530

    Change currency from dollar to rupee

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,15 +10,15 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
-export const currency = '$'
+export const currency = '₹'
 
 const App = () => {
 
-  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
 
-  useEffect(()=>{
-    localStorage.setItem('token',token)
-  },[token])
+  useEffect(() => {
+    localStorage.setItem('token', token)
+  }, [token])
 
   return (
     <div className='bg-gray-50 min-h-screen'>
